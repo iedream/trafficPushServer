de60555e2533877dbedf9a92a43acc43fd588cb3
Catherine Zhao <iedream@Catherines-MacBook-Pro.local>
2016-09-29 22:29:16 -0400

    new test

--- routes/users.js
+++ routes/users.js
@@ -6,33 +6,48 @@ var apnProvider = new apn.Provider({
     cert : __dirname + "/trafficPushCert.pem",
     key : __dirname + "/trafficPush.pem"
 });
-var deviceToken;
-var userInfo = {};
+
+var CronJob = require('cron').CronJob;
+
+//var deviceToken;
+//var userInfo = {};
     //"8A22947AEBE44234748012290E89DB4BB2C7EA9798B80E6ADCD86A0C99EFD055";
 
-/* GET users listing. */
-//'/sendNotification/:secondFromNow/:type/:source/:end/:name'
-router.get('/sendNotification/:time/:device/:source/:end/:name/:type', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-  var timePeriod = req.params.time * 1000;
-  deviceToken = req.params.device;
-  userInfo["source"] = req.params.source;
-  userInfo["end"] = req.params.end;
-  userInfo["name"] = req.params.name;
-  userInfo["type"] = req.params.type;
-  setTimeout(sendNotification, timePeriod);
 });
 
-function sendNotification() {
-  var notification = new apn.Notification();
-  notification.topic = "Catherine.traffic2";
-  notification.sound = "ping.aiff";
-  notification.alert = "did";
-  notification.contentAvailable = 1;
-  notification.payload = userInfo;
-  apnProvider.send(notification, deviceToken).then( result => {
-    var i = 4;
+/* GET users listing. */
+router.get('/sendNotification/:time/:device/:userInfo', function(req, res, next) {
+  res.send('respond with a resource');
+
+  var deviceToken = req.params.device;
+  var userInfo = req.params.userInfo;
+  var timeZoneContinent = req.params.time["continent"];
+  var timeZoneCity = req.params.time["city"];
+  var timeDaysInWeek = req.params.time["days"];
+  var time = req.params.time["clock"];
+  var timeString = '${time} * * ${timeDaysInWeek}';
+  var timeZone = '${timeZoneContinent}/${timeZoneCity}';
+
+  var job = new CronJob({
+    cronTime: timeString,
+    onTick: function() {
+      var notification = new apn.Notification();
+      notification.topic = "Catherine.traffic2";
+      notification.sound = "ping.aiff";
+      notification.alert = "did";
+      notification.contentAvailable = 1;
+      notification.payload = userInfo;
+      apnProvider.send(notification, deviceToken).then( result => {
+          var i = 4;
+      });
+    },
+    start: true,
+    timeZone: timeZone
   });
-}
+
+  job.start();
+});
 
 module.exports = router;
