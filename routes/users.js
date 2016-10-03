8199e42014dbcfbb4b336a514b15cb2cda726709
Catherine Zhao <iedream@Catherines-MacBook-Pro.local>
2016-10-03 00:37:11 -0400

    33 test

--- routes/users.js
+++ routes/users.js
@@ -71,7 +71,7 @@ router.post('/sendNotification/:device/', function(req, res, next) {
   var finalTimeString = convertToTimeString(timeString);
   var returnString =  "route: " + userInfo.name + " time: " + finalTimeString + " is successfully scheduled";
 
-  var key = req.params.device + req.params.name + timeString;
+  var key = req.params.device + userInfo.name + timeString;
   console.log("Save key: " + key);
   notificationDict[key] = job;
   job.start();
